99682fcb1cfb39db2ade11401c3714af8fb78c89
Alice088 <alien280579@gmail.com>
2024-03-16 11:07:58 +0300

    [+*-] made own telegram server, made Mars class, configured telegraph

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\Bots\MarsBot;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
